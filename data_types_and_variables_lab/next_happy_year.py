dba3f975e6ab9ae6ad5701718b7a8a6379747a68
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-02-12 12:40:43 -0300

    Add files via upload

--- data_types_and_variables_lab/next_happy_year.py
+++ data_types_and_variables_lab/next_happy_year.py
@@ -4,26 +4,11 @@ is_happy_year = False
 
 while not is_happy_year:
     year += 1
+
     str_year = str(year)
     set_year = set(str_year)
 
-    is_happy_year = len(str_year) == len(set_year)
+    if len(str_year) == len(set_year):
+        is_happy_year = True
 
 print(year)
-
-# OR
-# year = int(input())
-#
-# is_happy_year = False
-#
-# while not is_happy_year:
-#     year += 1
-#     str_year = str(year)
-#     set_year = set()
-#
-#     for i in range(len(str_year)):
-#         set_year.add(str_year[i])
-#
-#     is_happy_year = len(str_year) == len(set_year)      # if len(str_year) == len(set_year):
-#                                                         #  print(year)
-# print(year)
