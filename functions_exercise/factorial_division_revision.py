f959f579c8ba12882b02a899dc8a5f0bcb4629ad
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-02-15 14:05:57 -0300

    Add files via upload

--- functions_exercise/factorial_division_revision.py
+++ functions_exercise/factorial_division_revision.py
@@ -1,24 +1,28 @@
-# n = int(input())
-# result = 1
-# for i in range(1, n + 1):
-#     result *= i
-# print(result)
+def factorial(num):
+    return 1 if num == 0 or num == 1 else num * factorial(num - 1)
 
 
-def factorial_division(a, b):
-    factorial_a = 1
-    factorial_b = 1
-
-    for i in range(1, a + 1):
-        factorial_a *= i
-
-    for j in range(1, b + 1):
-        factorial_b *= j
-
-    result = f'{factorial_a / factorial_b:.2f}'
-    return result
+num_a = int(input())
+num_b = int(input())
+result = factorial(num_a) / factorial(num_b)
+print(f"{result:.2f}")
 
 
-current_a = int(input())
-current_b = int(input())
-print(factorial_division(current_a, current_b))
+
+# def factorial_division(a, b):
+#     factorial_a = 1
+#     factorial_b = 1
+#
+#     for i in range(1, a + 1):
+#         factorial_a *= i
+#
+#     for j in range(1, b + 1):
+#         factorial_b *= j
+#
+#     result = f'{factorial_a / factorial_b:.2f}'
+#     return result
+#
+#
+# current_a = int(input())
+# current_b = int(input())
+# print(factorial_division(current_a, current_b))
