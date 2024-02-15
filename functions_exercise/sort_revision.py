8350ed3cd0e25a731370e6b13cb93ea74edc7354
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-02-15 11:52:30 -0300

    Add files via upload

--- functions_exercise/sort_revision.py
+++ functions_exercise/sort_revision.py
@@ -1,4 +1,13 @@
-sequence = map(int, input().split(' '))
-sequence = sorted(sequence)
+def sorted_nums(nums):
+    return sorted(nums)
 
-print(sequence)
+
+current_nums = map(int, input().split(' '))
+print(sorted_nums(current_nums))
+
+# sequence = map(int, input().split(' '))
+# sequence = sorted(sequence)
+
+# print(sequence)
+
+
