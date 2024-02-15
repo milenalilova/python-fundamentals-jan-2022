2bc17ffc32a9d09f39e80c8d936c6eb9ab440df1
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-02-15 09:36:01 -0300

    Add files via upload

---
+++ functions_lab/grades_revision.py
@@ -0,0 +1,19 @@
+def print_grades(grade):
+    if grade < 3:
+        print('Fail')
+
+    elif grade < 3.50:
+        print('Poor')
+
+    elif grade < 4.50:
+        print('Good')
+
+    elif grade < 5.50:
+        print('Very Good')
+
+    elif grade < 6:
+        print('Excellent')
+
+
+current_grade = float(input())
+print_grades(current_grade)
