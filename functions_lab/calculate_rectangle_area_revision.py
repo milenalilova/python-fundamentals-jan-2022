eef31f59be30dc773eca7299f7b3d61a88c33669
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-02-15 10:32:35 -0300

    Add files via upload

---
+++ functions_lab/calculate_rectangle_area_revision.py
@@ -0,0 +1,8 @@
+def rectangle_area(width, height):
+    area = width * height
+    return area
+
+
+current_width = int(input())
+current_height = int(input())
+print(rectangle_area(current_width, current_height))
