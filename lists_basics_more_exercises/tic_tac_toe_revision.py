96e1beb462ba73e618786e01b27d48fe4f5fd138
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-02-20 12:39:31 -0300

    Add files via upload

---
+++ lists_basics_more_exercises/tic_tac_toe_revision.py
@@ -0,0 +1,54 @@
+first_line = input().split(' ')
+second_line = input().split(' ')
+third_line = input().split(' ')
+
+if first_line[0] == '1' and second_line[1] == '1' and third_line[2] == '1':
+    print("First player won")
+
+elif first_line[0] == '2' and second_line[1] == '2' and third_line[2] == '2':
+    print("Second player won")
+
+elif first_line[2] == '1' and second_line[1] == '1' and third_line[0] == '1':
+    print("First player won")
+
+elif first_line[2] == '2' and second_line[1] == '2' and third_line[0] == '2':
+    print("Second player won")
+
+elif first_line[0] == '1' and first_line[1] == '1' and first_line[2] == '1':
+    print("First player won")
+
+elif second_line[0] == '1' and second_line[1] == '1' and second_line[2] == '1':
+    print("First player won")
+
+elif third_line[0] == '1' and third_line[1] == '1' and third_line[2] == '1':
+    print("First player won")
+
+elif first_line[0] == '2' and first_line[1] == '2' and first_line[2] == '2':
+    print("Second player won")
+
+elif second_line[0] == '2' and second_line[1] == '2' and second_line[2] == '2':
+    print("Second player won")
+
+elif third_line[0] == '2' and third_line[1] == '2' and third_line[2] == '2':
+    print("Second player won")
+
+elif first_line[0] == '1' and second_line[0] == '1' and third_line[0] == '1':
+    print("First player won")
+
+elif first_line[1] == '1' and second_line[1] == '1' and third_line[1] == '1':
+    print("First player won")
+
+elif first_line[2] == '1' and second_line[2] == '1' and third_line[2] == '1':
+    print("First player won")
+
+elif first_line[0] == '2' and second_line[0] == '2' and third_line[0] == '2':
+    print("Second player won")
+
+elif first_line[1] == '2' and second_line[1] == '2' and third_line[1] == '2':
+    print("Second player won")
+
+elif first_line[2] == '2' and second_line[2] == '2' and third_line[2] == '2':
+    print("Second player won")
+
+else:
+    print("Draw!")
