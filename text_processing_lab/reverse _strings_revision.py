0779843edb3f57b74197338cf7d532cdf8e3ee51
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-02-29 10:00:25 -0300

    Add files via upload

---
+++ text_processing_lab/reverse _strings_revision.py
@@ -0,0 +1,10 @@
+word = input()
+
+while word != 'end':
+    reversed_word = ''
+    for ch in reversed(word):
+        reversed_word += ch
+
+    print(f"{word} = {reversed_word}")
+
+    word = input()
