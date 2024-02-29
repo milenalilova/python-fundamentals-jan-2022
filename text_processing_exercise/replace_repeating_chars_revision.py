db787d2aea23ecf5a9403b900fa2cb7edf91db93
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-02-29 13:22:27 -0300

    Add files via upload

--- text_processing_exercise/replace_repeating_chars_revision.py
+++ text_processing_exercise/replace_repeating_chars_revision.py
@@ -1,11 +1,23 @@
 text = input()
-new_text = ''
+new_text = text[0]
 
-for i in range(len(text) - 1):
-    if text[i] == text[i + 1]:
-        continue
-    else:
+for i in range(1, len(text)):
+    if text[i] != text[i - 1]:
         new_text += text[i]
-new_text += text[-1]
 
 print(new_text)
+
+
+# Variant 2
+
+# text = input()
+# new_text = ''
+#
+# for i in range(len(text) - 1):
+#     if text[i] == text[i + 1]:
+#         continue
+#     else:
+#         new_text += text[i]
+# new_text += text[-1]
+#
+# print(new_text)
