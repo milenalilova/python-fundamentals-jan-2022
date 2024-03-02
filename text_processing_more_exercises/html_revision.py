33ab9f76173500128638409517e5617809c06ebf
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-03-02 13:54:28 -0300

    Add files via upload

--- text_processing_more_exercises/html_revision.py
+++ text_processing_more_exercises/html_revision.py
@@ -8,3 +8,23 @@ comment = input()
 while comment != "end of comments":
     print(f"<div>\n    {comment}\n</div>")
     comment = input()
+
+
+# Variant 2
+
+# title = input()
+# content = input()
+# print(f"""
+# <h1>
+# {title}
+# </h1>
+# <article>
+# {content}
+# </article>
+# """, end='')
+# comment = input()
+# while comment != "end of comments":
+#     print('<div>')
+#     print(comment)
+#     print('</div>')
+#     comment = input()
