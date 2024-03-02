416bb3b8c3a58a5e4aa11cec4f0b5a3a1f2811a5
Milena Lilova <103578827+milenalilova@users.noreply.github.com>
2024-03-02 13:40:32 -0300

    Add files via upload

---
+++ text_processing_more_exercises/morse_code_translator_revision.py
@@ -0,0 +1,15 @@
+words = input().split(' | ')
+morse_dict = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H',
+              '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P',
+              '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
+              '-.--': 'Y', '--..': 'Z'}
+sentence = []
+for word in words:
+    phrase = ''
+    letters = word.split(' ')
+    for letter in letters:
+        if letter in morse_dict.keys():
+            phrase += morse_dict[letter]
+    sentence.append(phrase)
+
+print(' '.join(sentence))
